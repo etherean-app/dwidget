9de55d31f3b553c5bd8b4722877321f111bc0e0f
Alexey Elizarov <felex-ae@ya.ru>
2023-10-23 00:03:00 +0000

    feat: update widget template

---
+++ packages/template/src/components/Widget.tsx
@@ -0,0 +1,24 @@
+import { FunctionComponent } from "preact";
+import { useEffect } from "preact/hooks";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+
+export const Widget: FunctionComponent = () => {
+  const { address, isConnected } = useAccount();
+  const { connect, connectors } = useConnect();
+  const { disconnect } = useDisconnect();
+
+  useEffect(() => {
+    if (!isConnected) {
+      connect({ connector: connectors[0] });
+    }
+  }, []);
+
+  if (isConnected)
+    return (
+      <div>
+        Connected to {address}
+        <button onClick={() => disconnect()}>Disconnect</button>
+      </div>
+    );
+  return <button onClick={() => connect()}>Connect Wallet</button>;
+};
--- packages/template/vite.config.ts
+++ packages/template/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import path from "path";
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import { plugins } from "@dwidget/vite";
+
+import pkg from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-})
+  base: "dwidget/lido",
+  plugins: [preact(), plugins({ pkg })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+    },
+  },
+});
