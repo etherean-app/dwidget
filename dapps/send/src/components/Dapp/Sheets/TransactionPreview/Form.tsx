15e1604d6ff0fd991598b9bc60ae9ffc7cf6d0f0
Alexey Elizarov <felex-ae@ya.ru>
2023-11-13 16:20:28 +0300

    feat(send): gas price fetching

--- dapps/send/src/components/Dapp/Sheets/TransactionPreview/Form.tsx
+++ dapps/send/src/components/Dapp/Sheets/TransactionPreview/Form.tsx
@@ -1,4 +1,8 @@
-import { useSendTransaction, usePrepareSendTransaction } from "wagmi";
+import {
+  useSendTransaction,
+  usePrepareSendTransaction,
+  useFeeData,
+} from "wagmi";
 import { parseEther } from "viem";
 
 import { useStateMachine } from "@/providers/stateMachine";
@@ -15,14 +19,13 @@ import { useCallback } from "preact/hooks";
 export const Form = () => {
   const [state, send] = useStateMachine();
 
-  // const { data, isError, isLoading } = useFeeData();
-
-  // console.log(data, isError, isLoading);
+  const { data: dataFee, isSuccess: isSuccessFee } = useFeeData();
 
   const { config, error } = usePrepareSendTransaction({
+    gasPrice: dataFee?.gasPrice ?? undefined,
     to: state.context.recepient,
     value: state.context.amount ? parseEther(state.context.amount) : undefined,
-    enabled: !!state.context.amount,
+    enabled: !!state.context.amount && isSuccessFee,
   });
 
   const { data, isLoading, isSuccess, sendTransactionAsync } =
